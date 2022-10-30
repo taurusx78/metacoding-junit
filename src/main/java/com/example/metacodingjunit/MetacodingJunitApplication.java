f091131f49df392922afd66c1a0208e4a91d88f7
taurusx78 <taurusx@naver.com>
2022-10-30 12:50:16 +0900

    initail setting

---
+++ src/main/java/com/example/metacodingjunit/MetacodingJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.metacodingjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetacodingJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetacodingJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/metacodingjunit/MetacodingJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metacodingjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetacodingJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
